c5fa75f57c03b41206dc11946bcdbe529a4b1978
Iss Mneur <iss.mneur@issmneur.ca>
2016-10-01 20:35:51 -0600

    Integrate new scripts with main wxLauncher

--- build_deps-functions.cmake
+++ build_deps-functions.cmake
@@ -1,8 +1,11 @@
-function(configure_dep DEP_NAME BUILD_DIR BUILD_TYPE)
+function(configure_dep DEP_NAME BUILD_DIR BUILD_TYPE INSTALL_DIR)
 	message("Configure ${DEP_NAME} in: ${BUILD_DIR}")
 	file(MAKE_DIRECTORY ${BUILD_DIR})
 	execute_process(
-		COMMAND ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+		COMMAND ${CMAKE_COMMAND}
+		-DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+		-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+		..
 		WORKING_DIRECTORY ${BUILD_DIR}
 		RESULT_VARIABLE RES
 		)
@@ -14,7 +17,10 @@ endfunction()
 function(build_dep DEP_NAME BUILD_DIR BUILD_TYPE)
 	message("Building ${DEP_NAME} in: ${BUILD_DIR}")
 	execute_process(
-		COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE}
+		COMMAND ${CMAKE_COMMAND}
+		--build .
+	        --target INSTALL
+		--config ${BUILD_TYPE}
 		WORKING_DIRECTORY ${BUILD_DIR}
 		RESULT_VARIABLE RES
 	)
@@ -23,7 +29,7 @@ function(build_dep DEP_NAME BUILD_DIR BUILD_TYPE)
 	endif()
 endfunction()
 
-function(configure_build_dep DEP_NAME BUILD_DIR BUILD_TYPE)
-	configure_dep(${DEP_NAME} ${BUILD_DIR} ${BUILD_TYPE})
+function(configure_build_dep DEP_NAME BUILD_DIR BUILD_TYPE INSTALL_DIR)
+	configure_dep(${DEP_NAME} ${BUILD_DIR} ${BUILD_TYPE} ${INSTALL_DIR})
 	build_dep(${DEP_NAME} ${BUILD_DIR} ${BUILD_TYPE})
 endfunction()
--- build_deps.cmake
+++ build_deps.cmake
@@ -1,22 +1,18 @@
 set(GENERATOR "NMake Makefiles")
 
+set(3RD_PARTY_SOURCE_DIR ${CMAKE_BINARY_DIR})
+
 include(build_deps-functions.cmake)
+include(dirs.cmake)
 
-set(OPENAL "openal-soft-1.17.2")
-set(OPENAL_BUILD "${OPENAL}/build")
-set(SDL "SDL2-2.0.4")
-set(SDL_BUILD "${SDL}/build")
-set(WXMSW28 "wxMSW-2.8.12")
-set(WXMSW28_BUILD "${WXMSW28}/build")
-set(WXWIDGETS31 "wxWidgets-3.1.0")
-set(WXWIDGETS31_BUILD "${WXWIDGETS31}/build")
+set(OPENAL_BUILD "${3RD_PARTY_OPENAL}/build")
+set(SDL_BUILD "${3RD_PARTY_SDL2}/build")
+set(WXMSW28_BUILD "${3RD_PARTY_WXMSW28}/build")
+set(WXWIDGETS31_BUILD "${3RD_PARTY_WXWIDGETS31}/build")
 
-# do the actual build
-#include(build_deps-${CMAKE_SYSTEM_NAME}.cmake)
-
-configure_build_dep(OpenAL-Soft ${OPENAL_BUILD} "Release")
-configure_build_dep(SDL ${SDL_BUILD} "Release")
-configure_build_dep(wxMSW2.8 ${WXMSW28_BUILD} "Release")
-configure_build_dep(wxWidgets3.1 ${WXWIDGETS31_BUILD} "Release")
+configure_build_dep(OpenAL-Soft ${OPENAL_BUILD} "Release" ${3RD_PARTY_OPENAL})
+configure_build_dep(SDL ${SDL_BUILD} "Release" ${3RD_PARTY_SDL2})
+configure_build_dep(wxMSW2.8 ${WXMSW28_BUILD} "Release" ${3RD_PARTY_WXMSW28})
+configure_build_dep(wxWidgets3.1 ${WXWIDGETS31_BUILD} "Release" ${3RD_PARTY_WXMSW28})
 
 
